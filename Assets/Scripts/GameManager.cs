506752c4df62f2112c559980eab0a1636f9a106c
Khang <chikhang4566@gmail.com>
2023-03-03 03:59:25 +0700

    Modify - thay đổi sang cấu trúc queue

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,9 +8,10 @@ public class GameManager : MonoBehaviour
     private Transform Content;
     private GameObject FoodObject;
 
+    public static Queue<FoodColor> FoodQueue = new Queue<FoodColor>();
     double _timer = 3f;
     //color
-    Color[] _colors = new Color[] { Color.green, Color.red, Color.yellow };
+    Color[] _colors = new Color[] { Color.green, Color.red, Color.yellow, Color.white };
     private void Awake()
     {
         FoodPool = transform.Find("Pool").transform;
@@ -20,24 +21,39 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
-        if (_timer > 0)
+        if (_timer > 0 && FoodQueue.Count > 0)
         {
             _timer -= 1 * Time.deltaTime;
-            Debug.Log("_timer " + _timer);
         }
         else
         {
             GameObject obj = Instantiate(FoodObject, Content);
             obj.SetActive(true);
+            FoodColor foodColor = new FoodColor()
+            {
+                gameObject = obj,
+                color = _colors[Random.Range(0, 4)]
+            };
+            FoodQueue.Enqueue(foodColor);
+            obj.GetComponent<SpriteRenderer>().color = foodColor.color;
             _timer = 3f;
         }
+    }
+    public static void SetColor(GameObject gameObject, Color color)
+    {
+        Food food = gameObject.GetComponent<Food>();
+        food.SetColor(color);
+    }
+    public static Color GetColor(GameObject gameObject)
+    {
+        Food food = gameObject.GetComponent<Food>();
+        return food.GetColor();
     }
 }
 
 
-public enum FoodColor
+public class FoodColor
 {
-    green = 0,
-    red = 1,
-    yellow = 2
+    public GameObject gameObject;
+    public Color color;
 }
