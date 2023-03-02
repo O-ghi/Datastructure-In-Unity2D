f6fd5401ba3eaf272c77b9feb8185e958cf9a823
Khang <chikhang4566@gmail.com>
2023-03-02 16:29:13 +0700

    Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private Transform FoodPool;
+    private GameObject FoodObject;
+
+
+    //color
+    Color[] _colors = new Color[] { Color.green, Color.red, Color.yellow };
+    private void Awake()
+    {
+        FoodPool = transform.Find("Pool").transform;
+        FoodObject = transform.Find("Pool/Food").gameObject;
+    }
+
+    private void Update()
+    {
+
+    }
+}
+
+
+public enum FoodColor
+{
+    green = 0,
+    red = 1,
+    yellow = 2
+}
